530ba6f422f4ba6cf7f7f20930c572533592e6d0
Abdallah Elbastawesy <abdallahelbastawesy29@gamil.com>
2026-04-20 02:03:49 +0200

    authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API is running");
+  res.send("API is running 🚀");
 });
-const authRoutes = require("./routes/auth.routes");
 
+const authRoutes = require("./routes/auth.routes");
 app.use("/api/auth", authRoutes);
 
 module.exports = app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,17 @@
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./config/db");
 
-const authRoutes = require("./routes/auth.routes");
-const adminRoutes = require("./routes/admin.routes");
-const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-connectDB();
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+const authRoutes = require("./routes/auth.routes");
 
 app.use("/api/auth", authRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/user", userRoutes);
-
 
 module.exports = app;
