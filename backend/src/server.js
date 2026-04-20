dbd72e3a0a9753539ef20594afe8c6b07fd67181
Abdallah Elbastawesy <abdallahelbastawesy29@gamil.com>
2026-04-20 14:58:22 +0200

    authentication

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,8 @@
 require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const connectDB = require("./config/db");
+const app = require("./app");
 
-const authRoutes = require("./routes/auth.routes");
-const adminRoutes = require("./routes/admin.routes");
-const userRoutes = require("./routes/user.routes");
+const PORT = process.env.PORT || 5000;
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-connectDB();
-
-app.use("/api/auth", authRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/user", userRoutes);
-
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
