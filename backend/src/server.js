020297fa1966e56db3247964127072fc5f0f3c6b
Abdallah Elbastawesy <abdallahelbastawesy29@gamil.com>
2026-04-20 02:06:42 +0200

    authentication

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/auth.routes");
+const adminRoutes = require("./routes/admin.routes");
+const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-const authRoutes = require("./routes/auth.routes");
+connectDB();
 
 app.use("/api/auth", authRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/user", userRoutes);
+
 
 module.exports = app;
