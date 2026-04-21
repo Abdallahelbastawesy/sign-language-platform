7b349c3565eed9ff0e5fe80e8682b065d5168e83
Abdallah Elbastawesy <abdallahelbastawesy29@gamil.com>
2026-04-21 06:44:14 +0200

    authentication

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./src/config/db");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 const authRoutes = require("./src/routes/auth.routes");
 const adminRoutes = require("./src/routes/admin.routes");
 const userRoutes = require("./src/routes/user.routes");
@@ -11,6 +13,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Sign Language API",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./routes/*.js"], // مهم
+};
+
+const specs = swaggerJsdoc(options);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 // connect DB هنا (ممكن برضه يتحط في server.js)
 connectDB();
 
