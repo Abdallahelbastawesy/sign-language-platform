5985eb85d4234baf81e74542e47d9f7b06d02d23
Abdallah Elbastawesy <abdallahelbastawesy29@gamil.com>
2026-06-02 04:11:14 +0300

    fix swagger url

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ app.get("/docs", (req, res) => {
   <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
   <script>
     SwaggerUIBundle({
-      url: "/api-docs/json",
+      url: "https://sign-language-platform-cd91.vercel.app/api-docs/json",
       dom_id: '#swagger-ui',
       presets: [SwaggerUIBundle.presets.apis, SwaggerUIBundle.SwaggerUIStandalonePreset],
       layout: "StandaloneLayout"
